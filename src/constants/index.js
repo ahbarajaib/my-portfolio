43de7aee305eb83acaaf7dceb6ab4a9d54183b5b
Mohammed Ahbar <ahbarajaib@Mohammeds-MacBook-Pro-2.local>
2024-09-11 03:09:46 +0400

    Initial Commit

---
+++ src/constants/index.js
@@ -0,0 +1,75 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a results-driven MERN Stack Developer with a Master's in Computer Applications and a proven track record in building dynamic and high-performance web applications. With expertise in JavaScript, React.js, and Node.js, I excel at creating scalable solutions and delivering exceptional user experiences. My passion for coding and commitment to staying updated with the latest industry trends enable me to develop innovative web solutions that meet and exceed client expectations.`;
+
+export const ABOUT_TEXT = `I am a dedicated MERN Stack Developer with a Master's in Computer Applications and hands-on experience in designing and developing robust web solutions. Over the past few years, I’ve had the opportunity to work on a variety of projects, from e-commerce platforms to real-time dashboards, leveraging my skills in JavaScript, React.js, and Node.js. My approach is to combine a deep understanding of front-end and back-end technologies with a proactive mindset towards problem-solving and code optimization. I thrive in collaborative environments, enjoy tackling complex challenges, and am committed to delivering high-quality applications. Outside of coding, I stay active by keeping up with industry trends and continuously learning new technologies.`;
+
+export const EXPERIENCES = [
+  {
+    year: "August 2023 - Present",
+    role: "Full Stack Developer",
+    company: "Shahi Kohinoor",
+    description: `Developed multiple web applications using the MERN stack (MongoDB, Express.js, React.js, Node.js) for various clients. Collaborated directly with clients to gather requirements, design UI/UX, and implement backend functionalities. Implemented robust authentication and authorization mechanisms using JWT and integrated third-party APIs. Conducted thorough testing and debugging to ensure high performance and scalability.`,
+    technologies: ["JavaScript", "React.js", "Node.js", "MongoDB", "JWT"],
+  },
+  {
+    year: "June 2022 - July 2023",
+    role: "Freelance Full Stack Web Developer",
+    description: `Worked on diverse web projects utilizing the MERN stack. Engaged with clients to understand their needs, designed and implemented web solutions, and managed project timelines. Applied best practices for authentication, performance optimization, and integration of third-party services.`,
+    technologies: ["JavaScript", "React.js", "Node.js", "MongoDB"],
+  },
+  {
+    year: "October 2020 - May 2022",
+    role: "Software Developer",
+    company: "DXC Technology",
+    description: `Led the development of a MERN stack e-commerce grocery platform. Designed a responsive UI, built a secure back-end, and implemented user authentication with Redux for state management. Integrated third-party APIs and optimized application performance.`,
+    technologies: ["JavaScript", "React.js", "Node.js", "MongoDB", "Redux"],
+  },
+  {
+    year: "October 2018 - November 2019",
+    role: "Associate Software Developer",
+    company: "Infosys Limited",
+    description: `Contributed to various software development projects, focusing on front-end and back-end tasks. Collaborated with teams to design and implement features, debugged code, and optimized application performance.`,
+    technologies: ["JavaScript", "HTML", "CSS", "React.js"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Deira, Dubai, United Arab Emirates",
+  phoneNo: "+971 56 559 1596 ",
+  email: "ahbarajaib@gmail.com",
+};
